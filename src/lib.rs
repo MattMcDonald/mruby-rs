591365b5437c6e4a9b27981ddb544ec590a6fd9e
Matt McDonald <mail.matt.mcdonald@gmail.com>
2019-06-05 15:49:46 -0700

    running a little code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+#[macro_use]
+extern crate simple_error;
 extern crate mruby_sys;
 
+use std::error::Error;
 use mruby_sys::*;
 
 pub struct MRuby{
@@ -7,14 +10,29 @@ pub struct MRuby{
 }
 
 impl MRuby {
-     pub fn new() -> MRuby{
+     pub fn new() -> Result<MRuby, ()>{
         unsafe{
             let mrb_state = mrb_open();
-            
-            MRuby{
-                mrb_state : mrb_state
+            if mrb_state.is_null(){
+                return Err(())
             }
+            Ok(MRuby{
+                mrb_state : mrb_state
+            })
         }       
+    }
+
+    pub fn load_string(&mut self, string: &str) -> Result<i64, Box<Error>>{
+        unsafe{
+            let c_string = std::ffi::CString::new(string)?;
+            let result=  mrb_load_string(self.mrb_state, c_string.as_ptr());
+            match result.tt {
+                mruby_sys::mrb_vtype_MRB_TT_FIXNUM  => {
+                    return Ok(result.value.i)
+                }
+                _ => bail!("Only fixnums are supported right now")
+            }
+        }
     }
 }
 
@@ -31,7 +49,24 @@ impl Drop for MRuby {
 mod tests {
     use super::*;
     #[test]
-    fn can_make_and_drop() {
-        let _mruby = MRuby::new();
+    fn can_new_and_drop() {
+        let _mruby = MRuby::new().expect("Can't make a new mruby");
+    }
+
+    #[test]
+    fn can_add_1_and_1() -> Result<(), Box<Error>>{
+        let mut mruby = MRuby::new().expect("Can't make a new mruby");
+        let result = mruby.load_string("1 + 1")?;
+        assert_eq!(2, result);
+        Ok(())
+    }
+
+
+    #[test]
+    fn returns_error_if_unsupported_type() -> Result<(), Box<Error>>{
+        let mut mruby = MRuby::new().expect("Can't make a new mruby");
+        mruby.load_string("'2'").expect_err("Currently we only support ints");
+        mruby.load_string("1.1").expect_err("Currently we only support ints");
+        Ok(())
     }
 }
